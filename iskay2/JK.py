130a930338afa523384b959839a3c98a9efc2192
Patricio Gallardo <gallardo@nia-login02.scinet.local>
2022-09-28 18:08:29 -0400

    added JK lib

--- iskay2/JK.py
+++ iskay2/JK.py
@@ -20,3 +20,17 @@ def get_bs(df, params, rc):
         df_pws.append(df_pw)
 #    df_pws = pd.concat(df_pws)
     return df_pws
+
+
+class BS:
+    def __init__(self, df, params, rc):
+        df_pw_full_dataset = pwksz.pw_compute_ksz(df, params, rc)
+        df_pws = get_bs(df, params, rc)
+        curves = np.array([df['ksz_curve'].values
+                          for df in df_pws])
+        errorbar_std = curves.std(axis=0)
+        errorbar_percentiles = (np.percentile(curves, 50+68/2, axis=0) - 
+                                np.percentile(curves, 50-68/2, axis=0))/2
+        cov = np.cov(curves)
+        corr = np.corrcoef(curves)
+        
